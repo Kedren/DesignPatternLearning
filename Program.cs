dc56711941996de7bd6cea6d1ab2d9b60e95de50
Kedren <1696759698@qq.com>
2021-02-26 23:18:03 +0800

    Init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DesignPatternLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator=new Calculator();
+            // calculator.First(); //计算器第一版
+            calculator.Second();
+
+        }
+    }
+}
